e3cdc5e5be42dfd695898035ca9df7ac3161449c
Liz Fernandez <LizFernandez255>
2023-02-01 10:21:49 -0300

    Ejercicio 3-4-5 E/S 1.2.23

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,11 +1,17 @@
-/*
+/* LIZ CAROLINA FERNANDEZ
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	let nombreIngresado;
+	let mensaje;
+
+	nombreIngresado = document.getElementById("txtIdNombre").value;
+
+	mensaje = "Vos te llamas " + nombreIngresado;
+
+	alert(mensaje);
+
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,13 @@
-/* 
+/* LIZ CAROLINA FERNANDEZ
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let datoIngresado;
+
+	datoIngresado = prompt("Ingresar dato");
+
+	document.getElementById("txtIdNombre").value = datoIngresado;
 }
 
