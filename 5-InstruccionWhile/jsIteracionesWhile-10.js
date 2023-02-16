98ca86dce15a2e2d392f38dba3c3598335ba36a5
Jonathanporter442 <jonathanporter442@gmail.com>
2023-02-16 12:25:37 -0300

    prueba

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -11,7 +11,7 @@ hasta que el usuario quiera, mostrar:
 8-Promedios de negativos.
 9-Diferencia entre positivos y negativos, (positvos-negativos). */
 function mostrar() {
-	//declarar contadores y variables 
+	//declarar contadores y variables .
 	let respuesta;
 	let cantPositivos;
 	let cantNegativos;
