21f2f4d27ff7c962a1d7b687dfe76934754b41a9
LizFernandez255 <liz.f.sistemas@gmail.com>
2023-02-17 12:18:42 -0300

    bis9-16.2.23

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,19 +1,63 @@
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
+// function mostrar() {
+// 	// deck¿laro variable
+// 	let banderaDelPrimero;
+// 	let numMax;
+// 	let numMin;
+// 	let respuesta;
+// 	let numero;
+
+// 	// inicializo
+// 	banderaDelPrimero = true;
+// 	respuesta = "si";
+
+// 	while (respuesta == "si") {
+// 		numero = parseInt(prompt("Ingrese número"));
+// 		while (isNaN(numero)) {
+// 			numero = parseInt(prompt("Error. Ingrese un número:"));
+// 		}
+// 		if (banderaDelPrimero == true) {
+// 			numMax = numero;
+// 			numMin = numero;
+// 			// los primeros numeros estan aca
+// 			banderaDelPrimero = false;
+// 		}
+// 		else if (numMax < numero) {
+// 			numMax = numero;
+// 		}
+// 		else if (numMin > numero) { //numero ingresado < al minimo
+// 			numMin = numero;
+// 		}
+
+// 		respuesta = prompt("Desea continuar? si/no");
+// 	}
+
+// 	document.getElementById("txtIdMinimo").value = numMin;
+// 	document.getElementById("txtIdMaximo").value = numMax;
+
+// }
+
 function mostrar() {
 	// deck¿laro variable
 	let banderaDelPrimero;
+	let banderaDelPar;
+	let banderaDelNegativo;
 	let numMax;
 	let numMin;
 	let respuesta;
 	let numero;
+	let numNegativo;
+	let numPar;
 
 	// inicializo
 	banderaDelPrimero = true;
-	respuesta = "si";
+	banderaDelNegativo = true;
+	banderaDelPar = true;
+	respuesta = "s";
 
-	while (respuesta == "si") {
+	while (respuesta == "s") {
 		numero = parseInt(prompt("Ingrese número"));
 		while (isNaN(numero)) {
 			numero = parseInt(prompt("Error. Ingrese un número:"));
@@ -30,11 +74,22 @@ function mostrar() {
 		else if (numMin > numero) { //numero ingresado < al minimo
 			numMin = numero;
 		}
+		if (numero % 2 == 0){
+			if(banderaDelPar == true || numPar > numero){
+				numPar = numero;
 
-		respuesta = prompt("Desea continuar? si/no");
+			}
+		}
+
+		if (numero < 0){
+			if (banderaDelNegativo == true || numero > numNegativo){
+				numNegativo = numero
+			}
+		}
+		respuesta = prompt("Desea continuar? s/n");
 	}
-
-	document.getElementById("txtIdMinimo").value = numMin;
-	document.getElementById("txtIdMaximo").value = numMax;
-
+	document.write("El número máximo es: " + numMax + "</br>");
+	document.write("El número mayor negativo es: " + numNegativo + "</br>");
+	document.write("El número mínimo es: " + numMin + "</br>");
+	document.write("El número menor par es: " + numPar + "</br>");
 }
