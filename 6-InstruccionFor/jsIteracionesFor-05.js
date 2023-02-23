80f916fbc646ac8501788a0d58632e02fbdb12ce
Liz Fernandez <LizFernandez255>
2023-02-23 07:50:48 -0300

    parcial

--- 6-InstruccionFor/jsIteracionesFor-05.js
+++ 6-InstruccionFor/jsIteracionesFor-05.js
@@ -1,17 +1,67 @@
 /* al presionar el botón repetir el pedido de número hasta que ingresemos el 9 */
-function mostrar() 
-{
+// function mostrar() 
+// {
 
-	let repetir;
+// 	let repetir;
 
-	for (let i = 0 ; i < 10; i++) {
+// 	for (let i = 0 ; i < 10; i++) {
 
-		repetir = prompt("Para cortar ingrese " + 9);
-		
-		if(	repetir == 9){
-			break;
-		}
+// 		repetir = prompt("Para cortar ingrese " + 9);
 
-	}
+// 		if(	repetir == 9){
+// 			break;
+// 		}
 
-}//FIN DE LA FUNCIÓN
+// 	}
+
+// }//FIN DE LA FUNCIÓN
+
+/* For 5 bis, Realizar el programa que permita el ingreso de las notas (validar entre 0 y 10) , 
+el sexo (validar el sexo “f” o “m” o "b") de 7 alumnos, informar por console.log: 
+a) El promedio de las notas totales. 
+b) La nota más baja y el sexo de esa persona. 
+c) La cantidad de varones que su nota haya sido mayor o igual a 6.
+d) La cantidad de alumnos desaprobados (nota < 4) */
+
+function mostrar() {
+	let sexoAlumno;
+	let notaAlumno;
+	let totalNotas;
+	let banderaNotaBaja = 0;
+	let notaMasBaja;
+	let sexoNotaBaja;
+	let varMayorSeis;
+	let desaprobados;
+	let promedio;
+
+
+	sexoAlumno = prompt("Ingrese un sexo valido: 'f' o 'm' o 'b'");
+
+notaAlumno = parseInt(prompt("Ingrese la nota alumno: "));
+while (isNaN(notaAlumno) && notaAlumno < 0 && notaAlumno > 11) {
+	notaAlumno = parseInt(prompt("Error. Ingrese nota valida: "));
+}
+totalNotas = totalNotas + notaAlumno;
+if (banderaNotaBaja == 0) {
+	notaMasBaja = notaAlumno;
+	sexoNotaBaja = sexoAlumno;
+	banderaNotaBaja = 1;
+} else if (notaAlumno < notaMasBaja) {
+	notaMasBaja = notaAlumno;
+	sexoNotaBaja = sexoAlumno;
+}
+if (notaAlumno > 5 && (sexoAlumno == 'm' || sexoAlumno == 'M')) {
+	varMayorSeis = varMayorSeis + 1;
+}
+if (notaAlumno < 4) {
+	desaprobados = desaprobados + 1;
+}
+
+
+promedio = totalNotas / i;
+
+console.log("El promedio de las notas totales: " + promedio);
+console.log("La nota más baja es " + notaMasBaja + " y el sexo de esa persona es :" + sexoNotaBaja);
+console.log("Cantidad de varones que su nota haya sido mayor o igual a 6 es: " + varMayorSeis);
+console.log("Cantidad de alumnos desaprobados " + desaprobados);
+}
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,5 +1,89 @@
+function mostrar() {
 
-function mostrar()
-{
-	alert("uno");
+	let tipoProducto;
+	let precioProducto;
+	let unidadesProducto;
+	let marcaProducto;
+	let fabricanteProducto;
+	let contadorAlcohol;
+	let contadorBarbijo;
+	let contadorJabon;
+	let acumuladorAlcohol;
+	let acumuladorBarbijo;
+	let acumuladorJabon;
+	let unidadesCantidadMasBaratoAlcohol;
+	let fabricanteMasBaratoAlcochol;
+	let precioMasBaratoAlcohol;
+	let flagAlcohol;
+	let flagTipoConMasUnidades;
+	let promedioCompra;
+	let mayorTipo;
+
+	flagAlcohol = 0;
+	flagTipoConMasUnidades = 0;
+
+	contadorAlcohol = 0;
+	contadorBarbijo = 0;
+	contadorJabon = 0;
+	acumuladorAlcohol = 0;
+	acumuladorBarbijo = 0;
+	acumuladorJabon = 0;
+
+	for (let i = 0; i < 5; i++) {
+		tipoProducto = prompt("Ingrese el nombre del producto: barbijo, jabon o alcohol");
+		while (tipoProducto != "barbijo" && tipoProducto != "jabon" && tipoProducto != "alcohol") {
+			tipoProducto = prompt("Error- Ingrese el nombre del producto: barbijo, jabon o alcohol");
+		}
+
+		precioProducto = parseFloat(prompt("Ingrese el precio del producto unitario: "));
+		while (isNaN(precioProducto) || precioProducto < 100 || precioProducto > 300) {
+			precioProducto = parseFloat(prompt("Error- Ingrese el precio del producto: "));
+		}
+
+		unidadesProducto = parseInt(prompt("ingrese la cantidad de unidades del producto: "));
+		while (isNaN(unidadesProducto) || unidadesProducto < 1 || unidadesProducto > 1000) {
+			unidadesProducto = parseInt(prompt("Error- ingrese la cantidad de unidades del producto: "));
+		}
+
+		marcaProducto = prompt("Ingrse la marca del producto: ");
+		fabricanteProducto = prompt("ingrese el fabricante del producto: ");
+
+		switch (tipoProducto) {
+			case "alcohol":
+				if (flagAlcohol == 0 || precioProducto < precioMasBaratoAlcohol) {
+					fabricanteMasBaratoAlcochol = fabricanteProducto;
+					unidadesCantidadMasBaratoAlcohol = unidadesProducto;
+					precioMasBaratoAlcohol = precioProducto;
+					flagAlcohol = 1;
+				}
+				contadorAlcohol = contadorAlcohol + 1;
+				acumuladorAlcohol = unidadesProducto + acumuladorAlcohol;
+				break;
+			case "barbijo":
+				contadorBarbijo = contadorBarbijo + 1;
+				acumuladorBarbijo = acumuladorBarbijo + unidadesProducto;
+				break;
+			case "jabon":
+				contadorJabon = contadorJabon + 1;
+				acumuladorJabon = acumuladorJabon + unidadesProducto;
+				break;
+		}
+
+	}
+
+	if (acumuladorAlcohol > acumuladorJabon) {
+		mayorTipo = "el alcohol";
+		promedioCompra = acumuladorAlcohol / contadorAlcohol;
+	} else if (acumuladorJabon > acumuladorBarbijo) {
+		promedioCompra = acumuladorJabon / contadorJabon;
+		mayorTipo = " el jabon";
+	} else {
+		promedioCompra = acumuladorBarbijo / contadorBarbijo;
+		mayorTipo = " el barbijo";
+	}
+
+	console.log("Cantidad de unidades del mas barato de los alcohol: " + unidadesCantidadMasBaratoAlcohol + ". Su fabricante es: " + fabricanteMasBaratoAlcochol);
+	console.log("Unidades totales de jabon: " + acumuladorJabon);
+	console.log("El tipo con mas unidades es: " + mayorTipo);
+	console.log("El promedio de compra del tipo con mas unidades es: " + promedioCompra);
 }
